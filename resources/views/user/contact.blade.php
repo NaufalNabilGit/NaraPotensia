a88be66b6ddb9a5aefb170e6527f983c2382275d
NaufalNabilGit <naufal30679@gmail.com>
2026-05-30 15:00:51 +0700

    Fresh

---
+++ resources/views/user/contact.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.user')
+
+@section('header')
+    <style>
+        #hero{
+            background: url('{{asset('user/images/contact.png')}}') top center;
+            background-repeat: no-repeat;
+            width:100%;
+            background-size:cover;
+        }
+    </style>    
+@endsection
+
+@section('hero')
+    <h1>Contact Nara Potensia - Biro Psikologi</h1>
+    <h2>Bergabung dan Hubungi Kami</h2>
+@endsection
+
+@section('content')
+
+  <section id="contact">
+    <div class="container wow fadeInUp">
+      <div class="section-header">
+        <h3 class="section-title">Contact</h3>
+        <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque</p>
+      </div>
+    </div>
+
+    {{-- <div id="google-map" data-latitude="40.713732" data-longitude="-74.0092704"></div> --}}
+
+    <div class="container wow fadeInUp">
+      <div class="row justify-content-center">
+
+        <div class="col-lg-3 col-md-4">
+
+          <div class="info">
+            <div>
+              <i class="fa fa-map-marker"></i>
+              <p>Kota Semarang,
+                <br>Jawa Tengah</p>
+            </div>
+
+            <div>
+              <i class="fa fa-envelope"></i>
+              <p>narapotensia@gmail.com</p>
+            </div>
+
+            <div>
+              <i class="fa fa-phone"></i>
+              <p>0811-1111-1111</p>
+            </div>
+          </div>
+
+          <div class="social-links">
+            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+          </div>
+
+        </div>
+
+        <div class="col-lg-5 col-md-8">
+          <!-- <div class="form">
+            <div id="sendmessage">Your message has been sent. Thank you!</div>
+            <div id="errormessage"></div>
+            <form action="" method="post" role="form" class="contactForm">
+              <div class="form-group">
+                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                <div class="validation"></div>
+              </div>
+              <div class="text-center"><button type="submit">Send Message</button></div>
+            </form>
+          </div>
+        </div> -->
+
+        <!-- Peta Lokasi Kantor -->
+<div class="map-container" style="width: 100%; height: 400px;">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.781576064905!2d110.4616014749976!3d-7.034938992967006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708c37da6befcd%3A0xe459a3c3ab81fcb2!2sGreen%20Elang%20Residence!5e0!3m2!1sid!2sid!4v1748423229497!5m2!1sid!2sid" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</div>
+
+
+
+      </div>
+
+    </div>
+  </section><!-- #contact -->
+@endsection
